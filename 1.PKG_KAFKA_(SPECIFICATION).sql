858000188371d19173bc6cd78704605d1646a74a
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2024-06-11 13:21:00 +0300

    add README.MD

--- 1.PKG_KAFKA_(SPECIFICATION).sql
+++ 1.PKG_KAFKA_(SPECIFICATION).sql
@@ -1,17 +1,27 @@
 CREATE OR REPLACE PACKAGE PKG_KAFKA
 AS
 
-vURL_PROD varchar2(100) :='<host>:<port>/put_message';
-vURL_TEST varchar2(100) :='<host>:<port>/put_message';
+vURL_PROD varchar2(100) :='';
+vURL_TEST varchar2(100) :='';
 
 -- Отправка сообщений в KAFKA PROD
 FUNCTION SEND_MESSAGE(vTOPIC 	IN VARCHAR, 
 					cVALUE 		IN CLOB, 
-					vKEY 		IN VARCHAR2) RETURN NUMBER;
+					vKEY 		IN VARCHAR2 DEFAULT NULL) RETURN NUMBER;
 				
 -- Отправка сообщений в KAFKA TEST
 FUNCTION SEND_MESSAGE_TEST(vTOPIC 	IN VARCHAR, 
 						cVALUE 		IN CLOB, 
-						vKEY 		IN VARCHAR2) RETURN NUMBER;
+						vKEY 		IN VARCHAR2 DEFAULT NULL) RETURN NUMBER;
+					
+-- Отправка сообщений в KAFKA PROD
+PROCEDURE SEND_MESSAGE(vTOPIC 	IN VARCHAR, 
+					cVALUE 		IN CLOB, 
+					vKEY 		IN VARCHAR2 DEFAULT NULL);
+				
+-- Отправка сообщений в KAFKA TEST
+PROCEDURE SEND_MESSAGE_TEST(vTOPIC 	IN VARCHAR, 
+						cVALUE 		IN CLOB, 
+						vKEY 		IN VARCHAR2 DEFAULT NULL);
 				
 END PKG_KAFKA;
